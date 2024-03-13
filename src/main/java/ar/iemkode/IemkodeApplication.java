bcd3352a60882822f044d8becbefe9316ffb36f4
Igirod <ignaciogirodsd@gmail.com>
2024-03-13 17:13:51 -0300

    Initial commit for this project

---
+++ src/main/java/ar/iemkode/IemkodeApplication.java
@@ -0,0 +1,13 @@
+package ar.iemkode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IemkodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IemkodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/iemkode/IemkodeApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.iemkode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IemkodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
